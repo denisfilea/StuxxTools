17ebacf180c8f3c6ca17d8849e240813eb0b358d
denisfilea <denis.filea02@e-uvt.ro>
2022-12-05 21:05:31 +0200

    Basic names etc

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,24 +1,57 @@
-﻿namespace StuxxTools;
+﻿using System.Diagnostics;
+using Microsoft.Maui.Storage;
+
+namespace StuxxTools;
 
 public partial class MainPage : ContentPage
 {
-	int count = 0;
 
 	public MainPage()
 	{
 		InitializeComponent();
 	}
 
-	private void OnCounterClicked(object sender, EventArgs e)
+	private async void SelectionClick(object sender, EventArgs e)
 	{
-		count++;
+        if (formatPicker.SelectedIndex != -1)
+        {
+            var pickedAudio = await FilePicker.Default.PickAsync(new PickOptions
+            {
+                PickerTitle = "Select audio file",
+                FileTypes = new FilePickerFileType(
+                new Dictionary<DevicePlatform, IEnumerable<string>>
+                {
+                { DevicePlatform.WinUI, new [] { "*"+ (string)formatPicker.ItemsSource[formatPicker.SelectedIndex] } },
+                { DevicePlatform.Android, new [] { "audio/*" } },
+                { DevicePlatform.iOS, new[] { "public.audio" } },
+                { DevicePlatform.MacCatalyst, new[] { "public.audio" } }
+                })
+            });
 
-		if (count == 1)
-			CounterBtn.Text = $"Clicked {count} time";
-		else
-			CounterBtn.Text = $"Clicked {count} times";
 
-		SemanticScreenReader.Announce(CounterBtn.Text);
+            if (pickedAudio == null)
+                return;
+
+            var audioname = "File name: " + pickedAudio.FileName;
+
+            fileName.Text = audioname;
+        }
+        else
+        {
+            await DisplayAlert("Alert", "Pick an audio format before choosing a file.", "OK");
+        }
 	}
+
+    private void IndexChanged(object sender, EventArgs e)
+    {
+        if (formatPicker.SelectedIndex != -1)
+        {
+            infoText.Text = "Please pick a " + (string)formatPicker.ItemsSource[formatPicker.SelectedIndex] + " file.";
+        }
+    }
+
 }
+
+
+
 
